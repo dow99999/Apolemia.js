642876e2603cb69ac1d1fe739f5e1928d2795145
Diego Muñoz Jorquera <diegomujo@gmail.com>
2023-06-02 18:09:50 +0200

    fixed multi-tester

--- test/long_project/large_request_tester.js
+++ test/long_project/large_request_tester.js
@@ -2,7 +2,8 @@ const cp = require("child_process");
 
 (async () => {
   for(let i = 0; i < +process.argv[2]; i++) {
+    await new Promise(r => {setTimeout(r, 100)});
     console.log("Request [" + i + "]");
-    cp.spawn("apolemia.bat", ["py", "main.py"], { encoding : 'utf8' });
+    cp.spawn("apolemia.sh", ["python3", "main.py"], { encoding : 'utf8' });
   }
 })()
